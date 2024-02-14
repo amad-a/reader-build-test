dfc3af41ab122a9612f549ccae9093a88ba03be4
amad <amad@khajistan.com>
2024-02-14 09:34:35 -0500

    attempt for simple deformed uri error handling

--- web/viewer.mjs
+++ web/viewer.mjs
@@ -2409,7 +2409,7 @@ const PDFViewerApplication = {
     var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
     var jsonPayload = decodeURIComponent(window.atob(base64).split('').map(function(c) {
         return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
-    }).join(''));
+    }).join('')) || null;
 
     let parsedPayload = JSON.parse(jsonPayload);
 
